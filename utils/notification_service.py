1f2c9859ae7b8eb5a67e9887e115bfe75f6482d8
ydshieh <ydshieh@users.noreply.github.com>
2023-12-06 15:23:18 +0100

    fix

--- utils/notification_service.py
+++ utils/notification_service.py
@@ -1069,7 +1069,7 @@ if __name__ == "__main__":
 
     message = Message(title, ci_title, model_results, additional_results, selected_warnings=selected_warnings, prev_ci_results=prev_tables)
 
-    # # send report only if there is any failure (for push CI)
-    # if message.n_failures or (ci_event != "push" and not ci_event.startswith("Push CI (AMD)")):
-    #     message.post()
-    #     message.post_reply()
+    # send report only if there is any failure (for push CI)
+    if message.n_failures or (ci_event != "push" and not ci_event.startswith("Push CI (AMD)")):
+        message.post()
+        message.post_reply()
